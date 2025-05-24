58a0951354178cff2a4901467891b424bba37609
Lakunake <813luvhim@gmail.com>
2025-05-25 02:39:27 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Serve static and video files
+app.use(express.static(__dirname));
+app.use('/videos', express.static(path.join(__dirname, 'videos')));
+
+// Store the current video state globally (if needed)
+let currentVideoState = {
+  type: 'seek', // Default event type
+  time: 1555    // Default video start time (25:55)
+};
+
+// When a client connects, send them the current state
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  // Send the current video state to the new client
+  socket.emit('video-event', currentVideoState);
+
+  // Listen for video events and broadcast to other clients
+  socket.on('video-event', (data) => {
+    console.log(`Video event received: ${data.type} at ${data.time}`);
+    
+    // Update the global state
+    currentVideoState = data;
+
+    // Broadcast the event to all other clients (excluding the sender)
+    socket.broadcast.emit('video-event', data);
+  });
+
+  // When a client disconnects, log it
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+// Server listening on port 3000
+const PORT = 3000;
+server.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
---
+++ start.bat
@@ -0,0 +1,8 @@
+@echo off
+REM Change to your server folder
+cd /d C:\Korsan\WebDisplays
+
+REM Start the server
+node server.js
+
+pause
